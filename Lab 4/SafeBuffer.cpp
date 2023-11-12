2e49a8344cd2378c354429755b9570246c92b384
Josh <programmer10030@gmail.com>
2023-11-12 16:27:09 +0000

    SafeBuffer.cpp initial commit

---
+++ Lab 4/SafeBuffer.cpp
@@ -0,0 +1,21 @@
+#include "SafeBuffer.h"
+#include <iostream>
+#include <stdlib.h>
+#include "Event.h"
+
+//-------------------------------------------------------------------
+// Buffer (Class)
+//     The buffer is implemented with a Event class.
+//-------------------------------------------------------------------
+
+SafeBuffer::SafeBuffer(){
+  
+}
+
+void SafeBuffer::push(){
+
+}
+
+Event SafeBuffer::pop(){
+
+}
