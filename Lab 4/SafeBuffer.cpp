5ec900d0e32bff99b0543069442294c88a6bd30f
Josh <programmer10030@gmail.com>
2023-11-12 16:58:31 +0000

    implemented methods (unsafe)

--- Lab 4/SafeBuffer.cpp
+++ Lab 4/SafeBuffer.cpp
@@ -9,13 +9,17 @@
 //-------------------------------------------------------------------
 
 SafeBuffer::SafeBuffer(){
-  
+  theMutex = std::make_shared<Semaphore>(1);
+  semaphore = std::make_shared<Semaphore>(0);
 }
 
-void SafeBuffer::push(){
-
+void SafeBuffer::push(Event theEvent){
+  // use vector methods
+  items.push_back(theEvent);
 }
 
 Event SafeBuffer::pop(){
-
+  Event theEvent = items.back();
+  items.pop_back();
+  return theEvent;
 }
