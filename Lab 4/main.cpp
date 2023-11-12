c8e18f0974045fe7501e8507acd6ac6759615b61
Josh <programmer10030@gmail.com>
2023-11-12 17:53:25 +0000

    update, main.cpp should run without errors

--- Lab 4/main.cpp
+++ Lab 4/main.cpp
@@ -1,4 +1,5 @@
 #include "Barrier.h"
+#include "SafeBuffer.h"
 #include "Event.h"
 #include <iostream>
 #include <thread>
@@ -8,19 +9,17 @@
 static const int num_threads = 100;
 const int size=20;
 
-
 /*! \fn producer
     \brief Creates events and adds them to buffer
 */
 
-void producer(std::shared_ptr<SafeBuffer<std::shared_ptr<Event>> theBuffer, int numLoops){
+void producer(std::shared_ptr<SafeBuffer> theBuffer, int numLoops){
 
-  for(int i=0;i<numLoops;++i){
+  for(int i = 0; i < numLoops; ++i){    
     //Produce event and add to buffer
-    std::shared_ptr<Event> e= std::make_shared<Event>(i);
-    theBuffer.put(e);
-  }
-  
+    Event e;
+    theBuffer->push(e);
+  } 
 
 }
 
@@ -28,30 +27,40 @@ void producer(std::shared_ptr<SafeBuffer<std::shared_ptr<Event>> theBuffer, int 
     \brief Takes events from buffer and consumes them
 */
 
-void consumer(std::shared_ptr<SafeBuffer<std::shared_ptr Event>> theBuffer, int numLoops){
+void consumer(std::shared_ptr<SafeBuffer> theBuffer, int numLoops){
 
-  for(int i=0;i<numLoops;++i){
+  for(int i = 0; i < numLoops; ++i){
     //Produce event and add to buffer
-    std::shared_ptr<Event> e= theBuffer->get();
-    e->consume();
-  }
-  
+    Event e;
+    e = theBuffer->pop();
+    e.consume();
+  }  
 
 }
 
 int main(void){
-
-  std::vector<std::thread> vt(num_threads);
-  std::shared_ptr<SafeBuffer<std::shared_ptr<Event>> aBuffer( new Buffer<shared_ptr Event>(size));
+  std::vector<std::thread> producers(num_threads);
+  std::vector<std::thread> consumers(num_threads);
+  std::shared_ptr<SafeBuffer> aBuffer(new SafeBuffer());
+  
   /**< Launch the threads  */
-  int i=0;
-  for(std::thread& t: vt){
-    t=std::thread(updateTask,aBarrier,10);
+  
+  for(std::thread& p: producers){
+    p = std::thread(producer,aBuffer,10);
   }
+  for(std::thread& c: consumers){
+    c = std::thread(consumer,aBuffer,10);
+  }
+	  
   /**< Join the threads with the main thread */
-  for (auto& v :vt){
-      v.join();
+  
+  for (auto& p :producers){
+      p.join();
   }
-  std::cout << sharedVariable << std::endl;
+  for (auto& c :consumers){
+      c.join();
+  }
+  
+  //std::cout << std::endl;
   return 0;
 }
